a26bd3d1bf0d3a3048935d0a1e9ba033ca5fdd95
tlasnier <thibault .lasnier@gmail.com>
2014-02-25 02:38:59 +0100

    change password works

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.User;
+import org.mindrot.jbcrypt.BCrypt;
 import play.data.validation.Equals;
 import play.data.validation.MinSize;
 import play.data.validation.Required;
@@ -40,12 +41,16 @@ public class Users extends AbstractController {
     public static void changePassword(@Required(message = "You must precise your old password") String oldPassword,
                                       @MinSize(value = 6, message = "Must be at least 6 chars") String newPassword,
                                       @Equals(value = "newPassword", message = "Must be the same")String confirmation) {
+        User user = getConnectedUser();
+        if (!BCrypt.checkpw(oldPassword, user.getHashedPassword()))
+            validation.addError("oldPassword", "Wrong password!");
         if (validation.hasErrors()) {
             //Do not flash params!
         }
 
         else {
-            //TODO Get user an update
+            user.setHashedPassword(BCrypt.hashpw(newPassword, BCrypt.gensalt()));
+            user.save();
             flash.success("Your profile has been saved!");
             flash.keep();
         }

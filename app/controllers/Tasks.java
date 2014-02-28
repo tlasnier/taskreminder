5f4b3c9d1ddd7f9a30b6286b868cc7b66e477cdb
tlasnier <thibault .lasnier@gmail.com>
2014-02-28 16:37:41 +0100

    Forbidden access when try editing or deleting a task that you don't own.

--- app/controllers/Tasks.java
+++ app/controllers/Tasks.java
@@ -3,6 +3,7 @@ package controllers;
 import models.Task;
 import models.User;
 import play.data.validation.Valid;
+import play.mvc.Before;
 
 import java.util.Calendar;
 import java.util.List;
@@ -11,6 +12,15 @@ import java.util.List;
  * Created by Thibault on 25/02/14.
  */
 public class Tasks extends AbstractController {
+    @Before(only = {"edit", "delete"})
+    public static void checkOwner() {
+        Long id = params.get("id", Long.class);
+        Task task = Task.findById(id);
+        if (!task.getAuthor().equals(getConnectedUser()))
+            forbidden("You cannot edit or delete a task that is not yours!");
+
+    }
+
     public static void showUsersTasks() {
         User user = getConnectedUser();
         List<Task> tasks = Task.find("byAuthor", user).fetch();
